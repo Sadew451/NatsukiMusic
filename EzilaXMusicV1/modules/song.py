ff82012367883be22a0edfb650ef5339d3b8321c
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-08-12 05:16:15 -0700

    Update song.py

--- EzilaXMusicV1/modules/song.py
+++ EzilaXMusicV1/modules/song.py
@@ -50,7 +50,7 @@ def song(client, message):
     for i in message.command[1:]:
         query += " " + str(i)
     print(query)
-    m = message.reply("🔎 Finding the song...")
+    m = message.reply("🔎 𝔽𝕚𝕟𝕕𝕚𝕟𝕘 𝕥𝕙𝕖 𝕤𝕠𝕟𝕘...")
     ydl_opts = {"format": "bestaudio[ext=m4a]"}
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
@@ -70,13 +70,13 @@ def song(client, message):
         m.edit("❌ Found Nothing.\n\nTry another keywork or maybe spell it properly.")
         print(str(e))
         return
-    m.edit("Downloading the song ")
+    m.edit("𝔻𝕠𝕨𝕟𝕝𝕠𝕒𝕕𝕚𝕟𝕘 𝕥𝕙𝕖 𝕤𝕠𝕟𝕘")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = "**🎵 Uploaded by DaisyXMusic**"
+        rep = "**🎵 𝕌𝕡𝕝𝕠𝕒𝕕𝕖𝕕 𝕓𝕪 @EzilaXBot**"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
