a70bd1f6747b122632b4e8377a2cac9e9e54f603
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-05 07:26:54 -0700

    Updated

--- NatsukiMusic/services/callsmusic/__init__.py
+++ NatsukiMusic/services/callsmusic/__init__.py
@@ -1,6 +1,6 @@
 from pyrogram import Client
 
-from EzilaXMusicV1 import config
+from NatsukiMusic import config
 
 client = Client(config.SESSION_NAME, config.API_ID, config.API_HASH)
 run = client.run
