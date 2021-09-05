69c1fc752476bb7351cee5085ae43322ecaa2f29
Sadew Jayasekara <84616471+Sadew451@users.noreply.github.com>
2021-09-05 07:25:56 -0700

    Updated

--- NatsukiMusic/modules/ytsearch.py
+++ NatsukiMusic/modules/ytsearch.py
@@ -1,4 +1,4 @@
-# EzilaXMusic (Telegram bot project )
+# NatsukiMusic (Telegram bot project )
 # Copyright (C) 2021  Sadew Jayasekara
 
 # This program is free software: you can redistribute it and/or modify
